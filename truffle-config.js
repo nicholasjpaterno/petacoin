1dd33212eb610fb7ff1af6aab6ebaea4919ee62c
Nick Paterno <43416138+nicholasjpaterno@users.noreply.github.com>
2019-04-12 19:00:40 -0400

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -1,1 +1,21 @@
-module.exports = Object.assign({}, require('./__t2_truffle-configuration_.js'), {networks: {}});
+module.exports = {
+  // Uncommenting the defaults below 
+  // provides for an easier quick-start with Ganache.
+  // You can also follow this format for other networks;
+  // see <http://truffleframework.com/docs/advanced/configuration>
+  // for more details on how to specify configuration options!
+  /*
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*"
+    },
+    test: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*"
+    }
+  }
+  */
+};
