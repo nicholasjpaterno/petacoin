dc80aa831454c5b4180e986656ff5c9b162b8d0e
Nick Paterno <43416138+nicholasjpaterno@users.noreply.github.com>
2019-04-23 17:47:43 -0400

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -10,6 +10,11 @@ module.exports = {
       host: "127.0.0.1",
       port: 7545,
       network_id: "*"
+    },
+    test: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*"
     }
   }
 };
